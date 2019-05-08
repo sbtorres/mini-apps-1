cd7d994a65b8bd0515f1446541be376faffd86d8
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-05-08 11:48:42 -0700

    Refactor basic get request to use fs.sendFile

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,8 +2,21 @@ var express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  var options = {
+    root: __dirname + '/client/',
+  };
+
+  var fileName = req.params.name;
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Sent:', fileName);
+    }
+  });
 })
 
-app.listen(3700);
+app.listen(3700, () => {
+  console.log('Now listening on port 3700');
+});
 
