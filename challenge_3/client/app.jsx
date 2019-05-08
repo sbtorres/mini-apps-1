549ca840a9f9aff228823b5a4de4ca75d23fe43b
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-05-08 14:39:20 -0700

    Implement checkout button onClick functionality

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,20 +1,35 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {currentStep: 0};
+
+    this.onCheckoutButtonClick = this.onCheckoutButtonClick.bind(this);
+  }
+
+  onCheckoutButtonClick () {
+    console.log('clicked');
+    this.setState({currentStep: 1});
   }
 
   render() {
-    return (
-      <div>
-        <CheckoutButton></CheckoutButton>
-      </div>
-    )
+    if (this.state.currentStep === 0) {
+      return (
+        <div>
+          <CheckoutButton onCheckoutButtonClick={this.onCheckoutButtonClick}></CheckoutButton>
+        </div>
+      )
+    }
+
+    if (this.state.currentStep === 1) {
+      return (
+        <div>Test button functionality</div>
+      )
+    }
   }
 }
 
 var CheckoutButton = (props) => (
-  <button>Checkout</button>
+  <button onClick={props.onCheckoutButtonClick}>Checkout</button>
 )
 
 ReactDOM.render(<App />, document.getElementById("app"));
