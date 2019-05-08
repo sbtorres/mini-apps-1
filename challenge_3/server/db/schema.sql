59f1a9de8015942bb19dd173d1bd6ff49ffb5aec
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-05-08 16:24:46 -0700

    Implement schema.sql file to set up MYSQL database

---
+++ challenge_3/server/db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS checkout_app;
+
+CREATE DATABASE checkout_app;
+
+USE checkout_app;
+
+CREATE TABLE account_info (id INT AUTO_INCREMENT PRIMARY KEY, 
+                           name VARCHAR(255), 
+                           email VARCHAR(255), 
+                           password VARCHAR(255));
+
+CREATE TABLE address_info (id INT AUTO_INCREMENT PRIMARY KEY, 
+                           add_line_1 VARCHAR(255), 
+                           add_line_2 VARCHAR(255), 
+                           city VARCHAR(255),
+                           state VARCHAR(255),
+                           ship_zip INT, 
+                           account_id INT,
+                           FOREIGN KEY (account_id) REFERENCES account_info (id));
+
+CREATE TABLE billing_info (id INT AUTO_INCREMENT PRIMARY KEY, 
+                           credit_card_number INT, 
+                           exp_date DATE, 
+                           bill_zip INT,
+                           account_id INT, 
+                           FOREIGN KEY (account_id) REFERENCES account_info (id));
+
