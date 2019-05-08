d74ebd901037511dbf4e7d0bf731671a6610124b
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-05-08 12:24:16 -0700

    Update file path for server entry

--- challenge_3/server/server.js
+++ challenge_3/server/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 const PORT = 2500;
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'src', 'public')));
 
 app.listen(PORT, () => {
   console.log(`Server is now listening on port: ${PORT}!`);
