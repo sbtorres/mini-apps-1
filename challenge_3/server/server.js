e404dfdb8b5f9ed334821be91ebb04c7ce116e41
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-05-08 12:12:24 -0700

    Basic get request now serves index.html to client

--- challenge_3/server/server.js
+++ challenge_3/server/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const path = require('path');
+
+
 const app = express();
 const PORT = 2500;
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.listen(PORT, () => {
   console.log(`Server is now listening on port: ${PORT}!`);
