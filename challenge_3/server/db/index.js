e051f357e308f7b7439f8babbecdf309fc6eef11
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-05-08 18:22:03 -0700

    Implement makePurchase function to post to database

--- challenge_3/server/db/index.js
+++ challenge_3/server/db/index.js
@@ -8,8 +8,43 @@ var connection = mysql.createConnection({
 });
 
 var createAccount = (reqBody, callback) => {
-  connection.query(`INSERT INTO account_info (name, email, password) VALUES ("${reqBody.name}", "${reqBody.email}", "${reqBody.password}");`);
-  callback(null, `Successfully posted account information for ${reqBody.name}`);
+  connection.query(`INSERT INTO account_info (name, email, password) 
+                    VALUES ("${reqBody.accountInfo.name}", "${reqBody.accountInfo.email}", "${reqBody.accountInfo.password}");`, (err, results) => {
+                      if (err) {
+                        console.log('in err 1' + err);
+                        callback(err);
+                        return;
+                      } else {
+                        console.log(reqBody);
+                        connection.query(`INSERT INTO address_info (add_line_1, add_line_2, city, state, ship_zip, account_id) 
+                        VALUES ("${reqBody.addressInfo.addLine1}", "${reqBody.addressInfo.addLine2}", "${reqBody.addressInfo.city}", "${reqBody.addressInfo.state}", ${reqBody.addressInfo.shipZip}, 
+                        (SELECT id FROM account_info WHERE name = "${reqBody.accountInfo.name}"));`, (err, results) => {
+                          if (err) {
+                            console.log('in err 2' + err);
+                            callback(err);
+                            return;
+                          } else {
+                            connection.query(`INSERT INTO billing_info (credit_card_number, exp_date, bill_zip, account_id)
+                            VALUES ("${reqBody.billingInfo.creditCardNum}", "${reqBody.billingInfo.expDate}", ${reqBody.billingInfo.billingZip}, 
+                            (SELECT id FROM account_info WHERE name = "${reqBody.accountInfo.name}"));`, (err, results) => {
+                              if (err) {
+                                console.log('in err 3' + err);
+                                callback (err);
+                              } else {
+                                console.log('in success');
+                                callback(null, `Successfully posted account information for ${reqBody.accountInfo.name}`);
+                              }
+                            })
+                          }
+                        })
+                      }
+                    })
+
+
+
+
+  
+
 }
 
 connection.connect();
