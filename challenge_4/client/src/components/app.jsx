022b01939aba77c580e0f7a85f40256a16586c39
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-05-09 10:52:07 -0700

    Complete initial environment set-up using webpack

---
+++ challenge_4/client/src/components/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Test React</div>
+        <div>More React</div>
+      </div>
+    )
+  }
+}
+
+export default App;
+
--- challenge_4/server.js
+++ challenge_4/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-app.use(express.static('./public'));
+app.use(express.static('./client/dist'));
 
 app.listen(PORT, () => {console.log(`Now listening on port ${PORT}!`)});
 
