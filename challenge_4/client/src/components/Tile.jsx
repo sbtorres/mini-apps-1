6991c9502ab6457fb58f23ca3a3bf01226c70169
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-05-09 12:16:02 -0700

    Update board visual to be more realistic

--- challenge_4/client/src/components/Tile.jsx
+++ challenge_4/client/src/components/Tile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 var Tile = (props) => (
   <React.Fragment>
-    <div>Row: {props.row} Col: {props.col}</div>
+    <div className="tile"></div>
   </React.Fragment>
 )
 
